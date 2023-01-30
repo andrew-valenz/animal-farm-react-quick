16357ace57cf402bd83a68a479d40ef712452e12
ANDREW VALENZUELA <ANDREWJV93@GMAIL.COM>
2023-01-30 09:31:59 -0800

    mapped through the animals on app js and passed the props through main

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
-
 import './App.css';
-
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import Main from './components/Main/Main';
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,6 +1,16 @@
 import React from 'react';
+import background from '../../background.png';
 import './Main.css';
+import Animal from '../Animal/Animal.js';
+import animals from '../../data.js';
 
 export default function Main() {
-  return <main></main>;
+  return (
+    <main style={{ background: `url("${background}")` }}>
+      {animals.map((animal) => (
+        <Animal key={animal.name} prop={animal} />
+      ))}
+      ;
+    </main>
+  );
 }
